9e93cf11b79642ed7123bd2c19ef16871526eb94
joondae <68300257+joondae@users.noreply.github.com>
2020-11-12 09:30:12 -0800

    added some functions and events in main.js
    changes href's for index.html

--- public/main.js
+++ public/main.js
@@ -1,9 +1,22 @@
 //function syntax: function functionName () {}
+function unloadContent() {
+  $("#p1").html("");
+}
+
 function changeSomeHtml() {
   //very common for all websites
-  document.getElementById("p1").innerHTML = "THIS IS THE NEW CONTENT";
+  $("#p1").html("DOUBLE CLICK FOR DISAPPEARING ACT");
+};
+
+function undoDisappearingAct() {
+  $("#p1").html("poof");
 };
 
 //Events
-//$(idName) has same functionality as .getElementById but has more features
-$("p1").click(changeSomeHtml);
+//$(idName) has same functionality as document.getElementById but has more features
+$("#p1").click(changeSomeHtml);
+$("#p1").dblclick(unloadContent);
+$("#p1").mouseleave(undoDisappearingAct);
+
+//ajax calls:
+//$.ajax({url: "/pages/splash.html", success: loadContent}); 
