d35f0de7116d664880ace149bff3de2af5a9f9ed
joondae <68300257+joondae@users.noreply.github.com>
2020-12-03 05:45:16 -0800

    6 - Debugging, RSS Feeds, Favicon, Cookie
    code for #1, #3, & #4

--- public/hobby.js
+++ public/hobby.js
@@ -1,3 +1,76 @@
+function loadWIREDRss(result) {
+  result = xmlToJson(result);
+  var items = result['rss']['channel']['item'];
+  console.log(items);
+  //var rand = Math.round(Math.random() * 1000000) % 50;
+  var rand = Math.round(Math.random() * (Object.keys(items).length - 1)) % 50;
+  //console.log(rand);
+
+  var htmlStr = "";
+  for(var i = 0; i < items.length; i++) {
+    if(rand == i) {
+      htmlStr += '<img src="' + items[i]['media:thumbnail']['@attributes']['url'] + '"style="max-width:100%;max-height:100%;" alt="' + 'WIRED image of the day">';
+
+      //console.log(items[i]['media:thumbnail']['@attributes']['url']);
+    }
+  }
+
+  $("#RSSContent").html(htmlStr);
+}
+
+// Changes XML to JSON
+function xmlToJson(xml) {
+
+	// Create the return object
+	var obj = {};
+
+	if (xml.nodeType == 1) { // element
+		// do attributes
+		if (xml.attributes.length > 0) {
+		obj["@attributes"] = {};
+			for (var j = 0; j < xml.attributes.length; j++) {
+				var attribute = xml.attributes.item(j);
+				obj["@attributes"][attribute.nodeName] = attribute.nodeValue;
+			}
+		}
+	} else if (xml.nodeType == 3) { // text
+		obj = xml.nodeValue;
+	}
+
+	// do children
+	if (xml.hasChildNodes()) {
+		for(var i = 0; i < xml.childNodes.length; i++) {
+			var item = xml.childNodes.item(i);
+			var nodeName = item.nodeName;
+			if (typeof(obj[nodeName]) == "undefined") {
+				obj[nodeName] = xmlToJson(item);
+			} else {
+				if (typeof(obj[nodeName].push) == "undefined") {
+					var old = obj[nodeName];
+					obj[nodeName] = [];
+					obj[nodeName].push(old);
+				}
+				obj[nodeName].push(xmlToJson(item));
+			}
+		}
+	}
+	return obj;
+};
+
+
+function requestWIREDRss() {
+  $.ajax({
+    dataType: "xml",
+    url: "https://cors-anywhere.herokuapp.com/https://www.wired.com/feed/category/photo/latest/rss",
+    success: loadWIREDRss
+  });
+}
+
+function requestRSSFeeds() {
+  requestWIREDRss();
+  //add other rss requests here
+}
+
 function requestBlogArticleContent() {
   //load blogArticles.json
   $.ajax({
@@ -28,3 +101,4 @@ function loadBlogArticleContentJSONAsHTML(result) {
 }
 
 $("#blogArticleButton").click(requestBlogArticleContent);
+$("#WIREDButton").click(requestRSSFeeds);
--- public/main.js
+++ public/main.js
@@ -1,19 +1,38 @@
+//Website Visit Counter
+function websiteVisitCounter() {
+  var cookie = document.cookie;
+  if(!cookie) {
+    cookie = 1;
+  } else {
+    cookie++;
+  }
+  document.cookie = cookie;
+  $(".visitCounter").html("Visit Count: " + cookie);
+}
+
 //Functions
 //function syntax: function functionName () {}
 function changeBackground() {
   if(document.getElementById("background_image").src == "https://mir-s3-cdn-cf.behance.net/project_modules/max_1200/e7d2bd61228185.5a67a07360e75.gif") {
     //change background
-    document.getElementById("background_image").src = "https://i.imgur.com/VKCh9Hc.gif";
+    document.getElementById("background_image").src = "https://firebasestorage.googleapis.com/v0/b/maxwebsite-9bba2.appspot.com/o/VKCh9Hc.gif?alt=media&token=926f91df-4df5-4d7d-899e-27adf177af25";
+
+    //directly linking to firebase storage images using gs:// seems impossible
+    //document.getElementById("background_image").src = "gs://maxwebsite-9bba2.appspot.com/VKCh9Hc.gif";
 
     //change text colors
     document.getElementById("website_repo_link").style.color = "#A9A9A9";
     document.getElementById("tech_spec_link").style.color = "#A9A9A9";
     document.getElementById("github_repo_link").style.color = "#F08080";
-    document.getElementById ("p1").style.color = "#728C69";
+    document.getElementById("p1").style.color = "#728C69";
+    document.getElementById("p2").style.color = "#F0B30C"
 
     //change dark/light mode button icon (Jasmine could add the moon but couldn't remove the sun)
     //document.getElementById("button").removeClas("far fa-sun").addClass("fas fa-moon-stars");
     //document.getElementById("button").html("<button id="button"><i class="fas fa-moon-stars"></i></button>")
+
+    //change favicon
+    document.getElementById("favicon").href = "favicon2.ico";
 
     //change dark/light mode button & social icons to black (Jasmine thinks there's a more efficient way to do this but she failed)
     document.getElementById("button").style.color = "black";
@@ -23,7 +42,7 @@ function changeBackground() {
     document.getElementById("linkedin_button").style.color = "black";
 
     //change footer to light gray
-    document.getElementById("social-links").style.backgroundColor = "rgb(216,216,216,0.8)";
+    document.getElementById("social-links").style.backgroundColor = "rgba(216,216,216,0.8)";
   }
   else /*if currently in light mode*/ {
     //change background
@@ -34,15 +53,19 @@ function changeBackground() {
     document.getElementById("tech_spec_link").style.color = "rgb(255,20,147)";
     document.getElementById("github_repo_link").style.color = "rgb(127,255,0)";
     document.getElementById("p1").style.color = "rgb(255,20,147)";
+    document.getElementById("p2").style.color = "rgb(245,197,66)";
+
+    //change favicon
+    document.getElementById("favicon").href = "favicon.ico";
 
     //change dark/light mode button icon (Jasmine couldn't figure this out)
     document.getElementById("button").removeClass("far fa-moon").addClass("far fa-sun");
 
     //change dark/light mode button to white
-    document.getElementById ("button").style.color = "white";
+    document.getElementById("button").style.color = "white";
 
     //change footer to light pink
-    document.getElementById ("social-links").style.backgroundColor = "rgb(255,192,203,0.2)";
+    document.getElementById("social-links").style.backgroundColor = "rgba(255,192,203,0.2)";
 
     //change all social icons to white
     document.getElementById("instagram_button").style.color = "white";
@@ -73,7 +96,7 @@ function goToLinkedin (){
 
 
 function goToGameContent() {
-  console.log("TESTING");
+  //console.log("TESTING");
   location.href = "pages/game/game.html";
 }
 
@@ -89,7 +112,7 @@ function unloadContent() {
 
 //Events
 //$(idName) has same functionality as document.getElementById but has more features
-//$("game_button_home_page").click(goToGameContent);
+$(document).ready(websiteVisitCounter);
 $("#button").click(changeBackground);
 $("#instagram_button").click(goToInstagram);
 $("#facebook_button").click(goToFacebook);
